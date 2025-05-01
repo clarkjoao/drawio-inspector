fb6f17edb269a8d084436862f16c5465bdfa1f73
clarkjoao <clark.joao@gmail.com>
2025-05-01 17:56:20 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { TabsDemo } from "./components/tabs";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <TabsDemo />
+    </>
+  );
+}
+
+export default App;
